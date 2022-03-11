a2be01fefa38c5b0541c8820b78694d3af81a687
AlX-kim <ekipmurkor@gmail.com>
2022-03-11 05:53:05 -0800

    prints alphabet excepts q and e

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+
+/**
+* main - Evaluating random numbers
+*
+* Return: Always 0 (Success)
+*/
+
+int main(void)
+{
+char a = 'a';
+
+if (a != 'q' && 'e')
+{
+while (a <= 'z')
+{
+putchar (a);
+}
+putchar ('\n');
+return (0);
+}
