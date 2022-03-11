ccfb448066c4c2b4bcf44f0bd5a8dd9bbac2122d
AlX-kim <ekipmurkor@gmail.com>
2022-03-11 06:00:54 -0800

    prints alphate except q and e

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -9,14 +9,11 @@
 
 int main(void)
 {
-char a = 'a';
-
-if (a != 'q' && 'e')
-{
-while (a <= 'z')
-{
+char a;
+for (a = 'a'; a <= 'z'; a++)
+if (!= 'e' && c != 'q')
 putchar (a);
-}
 putchar ('\n');
 return (0);
 }
+
