accb4330774c5a74e823080c87acc2d6feda32ca
AlX-kim <ekipmurkor@gmail.com>
2022-03-11 04:54:12 -0800

    logic structures

--- 0x01-variables_if_else_while/1-last_digit.c
+++ 0x01-variables_if_else_while/1-last_digit.c
@@ -20,7 +20,7 @@ last_digit = n % 10;
 printf("Last digit of %d is %d and is ", n, last_digit);
 if (last_digit > 5)
 {
-printf(" greater than 5\n");
+printf("greater than 5\n");
 }
 else if (last_digit  == 0)
 {
