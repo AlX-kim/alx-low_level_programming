b5d76d6d467eb38760ea430d415d227e4e56230b
AlX-kim <ekipmurkor@gmail.com>
2022-03-11 10:08:14 -0800

    combination of numbers

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -15,15 +15,14 @@ for (tens = 0; tens <= 9; tens++)
 for (ones = tens + 1; ones <= 9; ones++)
 {
 putchar(tens + '0');
-																		putchar(ones + '0');
-																		if (tens < 8)
-																		{
-																		putchar(',');	
-																		putchar(' ');
-																		}															
-																		}
+putchar(ones + '0');
+if (tens < 8)
+{
+putchar(',');	
+putchar(' ');
+}
+}
 }
 putchar('\n');
-
 return (0);
 }
