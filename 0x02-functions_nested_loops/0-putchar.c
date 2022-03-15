bec55f8c3f89bbd79f95b0e38e09f70387a30e32
AlX-kim <ekipmurkor@gmail.com>
2022-03-15 07:46:37 -0700

    nested functions

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,10 @@
 #include "main.h"
 /**
-* _putchar - writes the character c to stdout
-* @c: The character to print
+* main -print -putchar
 *
-* Return: On success 1.
-* On error, -1 is returned, and errno is set appropriately.
+* Return:always
 */
-int main (void)
+int main(void)
 {
 _putchar('-');
 _putchar('p');
@@ -16,7 +14,6 @@ _putchar('c');
 _putchar('h');
 _putchar('a');
 _putchar('r');
-_putchar('n');
 _putchar('\n');
 return (0);
 }
