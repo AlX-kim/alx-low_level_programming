784e2a19074326c0aee8358ece54dd7f1f5b3465
AlX-kim <ekipmurkor@gmail.com>
2022-03-17 06:01:16 -0700

    nested functions

--- 0x04-more_functions_nested_loops/10-print_triangle.c
+++ 0x04-more_functions_nested_loops/10-print_triangle.c
@@ -17,9 +17,9 @@ for (x = 1; x <= size; x++)
 for (y = 1; y <= size; y++)
 {
 if (y <= size - x)
-																		_putchar(' ');
-																		else
-																		_putchar('#');
+_putchar(' ');
+else
+_putchar('#');
 }
 _putchar('\n');
 }
