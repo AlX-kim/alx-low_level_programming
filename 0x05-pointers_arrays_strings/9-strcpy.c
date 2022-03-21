ce2357a64215dc4483c9f1b6e152524e2cc2bf0f
AlX-kim <ekipmurkor@gmail.com>
2022-03-21 06:47:25 -0700

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/9-strcpy.c
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -1,22 +1,19 @@
 #include "main.h"
 
 /**
-* main - check the code
-*
-* _strcpy: copy strg to another string
-* @dest: character pointer
-* @src: character source
-*
-* Return: return pointer
+* *_strcpy - copy strg to another string
+* @dest: pointer
+* @src: poiter
+* Return: numbers dest
 */
 
 char *_strcpy(char *dest, char *src)
 {
 int i;
-for (i = 0; src[i]; i++)
+for (i = 0; src[i] != '\0'; i++)
 {
 dest[i] = src[i];
 }
-dest[i] = src[i];
+dest[i] = '\0';
 return (dest);
 }
