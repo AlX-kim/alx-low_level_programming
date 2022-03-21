04a2c7f01d70d5116152c5bbdda95a2d53e54f69
AlX-kim <ekipmurkor@gmail.com>
2022-03-21 06:00:29 -0700

    pointers, arrays and strings

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,3 +1,5 @@
+#ifndef MAIN_H
+#define MAIN_H
 int _putchar(char c);
 
 void reset_to_98(int *n);
@@ -19,3 +21,4 @@ void puts_half(char *str);
 void print_array(int *a, int n);
 
 char *_strcpy(char *dest, char *src);
+#endif
