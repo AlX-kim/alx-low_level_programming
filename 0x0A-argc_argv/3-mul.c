636d10b2de8dec0140a1468bd943ba48bc847820
AlX-kim <ekipmurkor@gmail.com>
2022-04-01 03:25:03 -0700

    argv and argc

--- 0x0A-argc_argv/3-mul.c
+++ 0x0A-argc_argv/3-mul.c
@@ -1,27 +1,28 @@
-#include <stdlib.h>
 #include <stdio.h>
+#include <stdlib.h>
+
 /**
-*main - multiplies two numbers
-*
-*@argc:arguement count
-*@argv:arguement vector
-*Return:0(success)
+* main - adds all positive numbers and prints it
+* @argc: argument count
+* @argv: argument vector
+* Return: 0 for success
 */
+
 int main(int argc, char *argv[])
 {
-int product = 0;
-int x, y;
-if (argc == 3)
+int i, res;
+res = 0;
+for (i = 0; i < argc; i++)
 {
-x = atoi(argv[1]);
-y = atoi(argv[2]);
-product = x * y;
-printf("%d\n", product);
+if (i > 0)
+{
+if (atoi(argv[i]) == 0)
+{
+return (printf("Error\n"), 1);
+}
+res += atoi(argv[i]);
+}
+}
+printf("%d\n", res);
 return (0);
-}
-else
-{
-printf("Error\n");
-return (1);
-}
 }
